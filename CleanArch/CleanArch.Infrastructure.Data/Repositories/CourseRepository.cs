bc3af14ee0fd6b45afd013a91abe47089a96db63
Windy Code <windijcode@outlook.com>
2020-02-27 22:06:28 +0700

    Commit implementation of Service and Repository GetCourses methods

---
+++ CleanArch/CleanArch.Infrastructure.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infrastructure.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infrastructure.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDBContext _context;
+
+        public CourseRepository(UniversityDBContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
