c7fb72c027f9d1c2bf4b0f64edbaf5b75e2bfb49
Windy Code <windijcode@outlook.com>
2020-02-28 15:40:54 +0700

    Courses service create funtionality

--- CleanArch/CleanArch.Application/ViewModels/CourseViewModel.cs
+++ CleanArch/CleanArch.Application/ViewModels/CourseViewModel.cs
@@ -5,6 +5,9 @@ namespace CleanArch.Application.ViewModels
 {
     public class CourseViewModel
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
         public IEnumerable<Course> Courses { get; set; }
     }
 }
