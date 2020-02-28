2e0956efc93a67476966e98320a486c3c22f467f
Windy Code <windijcode@outlook.com>
2020-02-28 15:18:24 +0700

    Commands, ComandHandlers, Updated Course repository, Cleaned up IoC container

--- CleanArch/CleanArch.Infrastructure.Data/Repositories/CourseRepository.cs
+++ CleanArch/CleanArch.Infrastructure.Data/Repositories/CourseRepository.cs
@@ -16,6 +16,12 @@ namespace CleanArch.Infrastructure.Data.Repositories
             _context = context;
         }
 
+        public void Add(Course course)
+        {
+            _context.Add(course);
+            _context.SaveChanges();
+        }
+
         public IEnumerable<Course> GetCourses()
         {
             return _context.Courses;
--- CleanArch/CleanArch.Infrastructure.IoC/DependencyContainer.cs
+++ CleanArch/CleanArch.Infrastructure.IoC/DependencyContainer.cs
@@ -1,11 +1,14 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.Services;
+using CleanArch.Domain.CommandHandlers;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
+using CleanArch.Infrastructure.Bus;
+using CleanArch.Infrastructure.Data.Context;
 using CleanArch.Infrastructure.Data.Repositories;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CleanArch.Infrastructure.IoC
 {
@@ -13,11 +16,18 @@ namespace CleanArch.Infrastructure.IoC
     {
         public static void RegisterSerivices(IServiceCollection services)
         {
+            // Domain InMemoryBus MediatR
+            services.AddScoped<IMediatorHandler, InMemoryBus>();
+
+            // Domain Handlers
+            services.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
+
             // Application layer
             services.AddScoped<ICourseService, CourseService>();
 
             // Infrastructure.Data layer
             services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<UniversityDBContext>();
         }
     }
 }
